6056d06ec1117758f19c91966b6cd34500cd20bc
Dhvanik Patel <pateldhvanik789@gmail.com>
2024-01-22 23:06:42 +0530

    added cookies concept

--- src/Routes/product.mjs
+++ src/Routes/product.mjs
@@ -3,7 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-	response.send([{id:123,name:"chiken breast",prize:12.99}]);
+   // console.log(request.headers.cookie);
+   response.cookie('Hello',"world",{signed:true});
+   console.log(request.cookies);
+   if (request.cookies.Hello && request.cookies.Hello === "world") { 
+      response.send([{ id: 123, name: "chiken breast", prize: 12.99 }]); 
+   }
+   else{
+      return response.status(403).send({msg:"Sorry.You need the correct cookies."});
+   }
 });
 
 export default router;
--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,17 @@
 import express from 'express';
 import routes from "./Routes/index.mjs";
+import cookieParser from 'cookie-parser';
 const app = express();
-const PORT= process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-
+app.use(cookieParser('hello world'));
 app.use(routes);
 
 app.get('/', (req, res) => {
+   res.cookie('Hello', 'world', { maxAge: 60000 });
    res.send(`Hello, World!`);
 });
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
    console.log(`listening on ${PORT}`);
 });
